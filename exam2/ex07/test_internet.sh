07f07d93aa7a81981d9f24ad58325a62f6c0e8df
steve-soares <steve.coubertin@gmail.com>
2019-11-08 16:55:39 +0100

    ajout de exam2

--- exam2/ex07/test_internet.sh
+++ exam2/ex07/test_internet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-now =$(date + "%Y-%m-%d:%H:%M:%S")
+now=$(date +"%Y-%m-%d:%H:%M:%S")
 curl -s -I www.google.com >/dev/null
 
 if [ $? == 0 ]; then
-    echo "{$now} internet OK" >> internet.log
+    echo "${now} internet ok" >> internet.log
 else
-    echo "{$now} internet FAIL" >> internet.log
+    echo "${now} internet FAIL" >> internet.log
 fi
